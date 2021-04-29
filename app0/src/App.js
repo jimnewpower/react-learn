ed5142e0350a0789ddb3955d13dadb6c74b87350
Jim Newpower <jimnewpower@gmail.com>
2021-04-29 15:52:01 -0600

    Add Switch component

--- app0/src/App.js
+++ app0/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import SwitchExample from './SwitchExample'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SwitchExample />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Here!
         </a>
       </header>
     </div>
